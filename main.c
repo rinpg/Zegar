7f099d40821f1128c778b9692507a7f8d6e6e877
M.Roman <michalkrzysztofroman@gmail.com>
2016-05-16 09:27:33 +0200

    dodano header do bliblioteki twi

--- main.c
+++ main.c
@@ -5,31 +5,11 @@
 #include "rtc.h"
 int main()
 {
-	seg_int();
-	sei();
-	twi_init_master();
 
-w1=12;
-w2=12;
-w3=12;
-w4=12;
+while(1)
+	{
 
-while(1){
-	rtc_get_time_s(&hour,&min,&sec);
-	if((sec%15==0)|(sec%15==1)){
-		rtc_get_temp(&i,&f);
-		w1=i/10;
-		w2=i%10;
-		w3=14;
-		w4=15;
-	}else{
-	w1=hour/10;
-	if(w1==0)w1=10;
-	w2=hour%10;
-	w3=min/10;
-	w4=min%10;
-}
-}
+	}
 }
 
 
